93acaa183ccad9529ff98f9cd9abbe37b6bdb155
Chernet Tafesse <120357658+Jrkusha5@users.noreply.github.com>
2024-02-11 14:05:00 -0800

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import {AppBar, Toolbar, Typography, Tabs, Tab} from '@mui/material'
+import {NavLink } from 'react-router-dom'
+
+const Header = () => {
+    const[value, setValue]= useState();
+
+  return (
+    <div>
+
+        <AppBar sx={{backgroundColor:'#232F3D'}} position='sticky'>
+            <Toolbar>
+            <Typography>Book Store</Typography>
+            <Tabs sx={{ml: 'auto'}}
+             textColor='inherit' indicatorColor='secondary' value={value}
+            onChange={(e,val)=>setValue(val)}>
+                <Tab LinkComponent={NavLink} to='/add' label='Add product'/>
+                <Tab LinkComponent={NavLink} to='/books' label='Books'/>
+                <Tab LinkComponent={NavLink} to='/about' label='About Us'/>
+            </Tabs>
+            </Toolbar>
+        </AppBar>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>saks is going to delivery something</div>
+  )
+}
+
+export default Home
